a5d394b987b771cbe891c691745f72f24a43f1ca
Lui86 <l.ruiz86@gmail.com>
2014-05-13 19:49:52 +0200

    Luis
    Login

---
+++ Luis/inicio.php
@@ -0,0 +1,51 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/jquery-1.10.2.js"></script>
+	<script src="js/jquery-ui-1.10.4.custom.js"></script>
+	<script type="text/javascript">
+	$(function(){
+		$("#imagen").hide();
+		
+	});
+	function runEffect() {
+		var selectedEffect = "explode";
+		var options = {};
+		$( "#imagen" ).show( selectedEffect, options, 1000);
+	};
+	window.onload=runEffect;
+	
+	</script>
+</head>
+<body>
+<div id="wrap">
+	<div id="header">
+		<img src="img/logo.png">
+	</div>
+	<div id="contenedor">
+		<div id="home-left"><img src="img/home_img1.jpg" id="imagen"></div>
+		<div id="home-right">
+			<form action="#" method="post" name="form-login">
+				<fieldset>
+					<legend><h2>Inicia sesi&oacute;n</h2></legend>
+					<p><label for="usuario">Usuario: <input type="text" id="usuario" name="usuario"></p>
+					<p><label for="password">Contrase&ntilde;a: </label><input type="password" id="password" name="password"></p>
+					<input type="submit" name="enviar" id="boton-login" value="Iniciar sesi&oacute;n">
+				</fieldset>
+				<p><a href="#">&#191;Olvidaste tu contrase&ntilde;a?</a></p>
+				<p><a href="#">&#191;Eres nuevo? Reg&iacute;strate</a></p>
+			</form>
+		</div>
+		
+	</div>
+	<div id="footer">
+		<span><a href="#">Sobre nosotros</a></span>
+		<span><a href="#">Condiciones</a></span>
+		<span><a href="#">Privacidad</a></span>
+		<span>&#64; 2014 Alumnos D.A.W.</span>
+	</div>
+</div>
+</body>
+</html>
